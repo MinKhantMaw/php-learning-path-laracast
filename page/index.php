a2aa8033a183c4add1f9fa8d8d3f3f2cbd9a959a
mkhant <minkhantmaw@onenex.co>
2025-08-23 20:12:59 +0630

    start pdo

--- page/index.php
+++ page/index.php
@@ -1,3 +1,32 @@
 <?php
 
-require 'index.view.php';
+require 'functions.php';
+// require 'router.php';
+
+
+
+$dsn = "mysql:host=127.0.0.1;port=3306;dbname=myapp;charset=utf8mb4";
+$username = "root";
+$password = "password";
+
+
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+
+
+$query = $pdo->prepare("SELECT * FROM posts");
+$query->execute();
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+// echo "<pre>";
+// print_r($posts);
+// echo "</pre>";
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
